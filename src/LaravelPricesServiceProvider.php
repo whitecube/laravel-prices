f00f52a75373dccedf559a0cb457fdf1e69fd0e9
Jonathan Rixhon <jonathan@whitecube.be>
2023-04-28 14:19:45 +0200

    Update LaravelPricesServiceProvider.php
    Added the config file publication method.
    Added merge config if the user doesn't publish the package's config file.

--- src/LaravelPricesServiceProvider.php
+++ src/LaravelPricesServiceProvider.php
@@ -8,6 +8,25 @@ class LaravelPricesServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishConfig();
+        }
+
+        if (!file_exists(config_path('prices.php'))) {
+            $this->mergeConfigFrom($this->getConfigPath(), 'prices');
+        }
+    }
+
+    private function publishConfig()
+    {
+        $path = $this->getConfigPath();
+        $this->publishes([$path => config_path('prices.php')], 'prices-config');
+    }
+
+    private function getConfigPath()
+    {
+        return __DIR__ . '/../config/prices.php';
     }
 }
